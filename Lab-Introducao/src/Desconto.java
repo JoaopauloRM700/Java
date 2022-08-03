d4d10779700ae8f446003b52e19be3edc8fa9873
Joao Paulo Reis Marques <44990001+JoaopauloRM700@users.noreply.github.com>
2022-08-03 17:19:38 -0400

    Create Desconto.java

---
+++ Lab-Introducao/src/Desconto.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormatSymbols;
+import java.util.Scanner;
+
+public class Desconto {
+
+	public static void main(String[] args) {
+		/*Para atrair mais clientes, uma loja de roupas oferece um desconto de 5% em compras de R$200,00 ou mais.
+		 *  Escreva um programa (classe Desconto) que lê o preço sem desconto de uma compra 
+		 *  e imprime o valor a ser pago pelo cliente (com duas casas decimais).*/
+		
+		
+		double valor = 0;
+		double valorComDesc;
+		//double desconto;
+		
+		DecimalFormatSymbols custom=new DecimalFormatSymbols();
+		custom.setDecimalSeparator('.');
+		
+		Scanner read = new Scanner(System.in);
+		valor = read.nextDouble();
+		//System.out.println(valor);
+		if(valor >= 200) {
+			//desconto = valor*(0.05);
+			//System.out.println(desconto);
+			valorComDesc = valor - ( valor * (0.05));
+			System.out.printf("%.2f",valorComDesc);
+		}
+		
+		read.close();
+
+	}
+
+}
